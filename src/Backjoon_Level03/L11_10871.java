a927855cd0762624274aa5f97b731a3a7f6477c3
proudofch <proudofch@gmail.com>
2020-09-06 14:30:38 +0900

    백준 - 단계별로 풀어보기 레벨3 - X보다 작은 수

---
+++ src/Backjoon_Level03/L11_10871.java
@@ -0,0 +1,34 @@
+package Backjoon_Level03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class L11_10871 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String[] input = br.readLine().split(" ");
+		
+		int n = Integer.parseInt(input[0]);
+		int x = Integer.parseInt(input[1]);
+		
+		String[] secondInput = br.readLine().split(" ");
+		
+		if(n == secondInput.length) {
+			for(int i=0; i<secondInput.length; i++) {
+				if(Integer.parseInt(secondInput[i]) < x) {
+					bw.write(secondInput[i]+" ");
+				}
+			}
+		}
+		
+		bw.close();
+		br.close();
+
+	}
+}
