6c31c6d3481d5c44965ad90188a013c2af261d3f
proudofch <proudofch@gmail.com>
2020-10-23 21:18:13 +0900

    백준 - 2845

---
+++ src/Backjoon_Group_Basic01/BOJ_02845.java
@@ -0,0 +1,29 @@
+package Backjoon_Group_Basic01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_02845 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int l = Integer.parseInt(st.nextToken());
+		int p = Integer.parseInt(st.nextToken());
+		int numberOfPeople = l * p;
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<5; i++) {
+			int participants = Integer.parseInt(st.nextToken());
+			sb.append(participants - numberOfPeople + " ");
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
+		
+	}
+}
