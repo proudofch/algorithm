071338259a3d406afcda6db75ba0c6c9481d9911
proudofch <proudofch@gmail.com>
2020-10-24 09:38:59 +0900

    백준 - 5554

---
+++ src/Backjoon_Group_Basic01/BOJ_05554.java
@@ -0,0 +1,23 @@
+package Backjoon_Group_Basic01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_05554 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int sum = 0;
+		
+		for(int i=0; i<4; i++) {
+			 sum += Integer.parseInt(br.readLine());
+		}
+		
+		System.out.println(sum / 60);
+		System.out.println(sum % 60);
+		
+		br.close();
+	}
+}
