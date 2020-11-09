ee243517a6ca42ec420a810223a5a7df2e7b3f59
proudofch <proudofch@gmail.com>
2020-11-09 23:40:28 +0900

    백준 - 2440

---
+++ src/Backjoon_Group_Basic01/BOJ_02440.java
@@ -0,0 +1,19 @@
+package Backjoon_Group_Basic01;
+
+import java.util.Scanner;
+
+public class BOJ_02440 {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int star = scanner.nextInt();
+		
+		for(int i=1; i<=star; i++) {
+			for(int j=star; j>=i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+}
