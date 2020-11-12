c2ded9d71971eb7406308d1c08b557a5f1eaa147
proudofch <proudofch@gmail.com>
2020-11-12 22:13:33 +0900

    백준 - 단계별로 풀어보기 5 - 2562, 2577, 10818

---
+++ src/Backjoon_Level05/L02_02562.java
@@ -0,0 +1,30 @@
+package Backjoon_Level05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class L02_02562 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] array = new int[9];
+		int max = Integer.MIN_VALUE;
+		int index = -1;
+		
+		for(int i=0; i<array.length; i++) {
+			
+			array[i] = Integer.parseInt(br.readLine());
+			max = Math.max(max, array[i]);
+			if(array[i] == max) {
+				index = i+1;
+			}
+			
+		}
+		
+		System.out.println(max+"\n"+index);
+		
+		br.close();
+	}
+}
