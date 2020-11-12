74bc45ccb1cd3fc2d7c13d61c13f57954f22f7e8
proudofch <proudofch@gmail.com>
2020-11-13 03:18:56 +0900

    백준 - 단계별로 풀어보기 5 - 1546, 3052, 4344, 8958

---
+++ src/Backjoon_Level05/L06_08958.java
@@ -0,0 +1,44 @@
+package Backjoon_Level05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class L06_08958 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		String[] array = new String[n];
+		int sum = 0;
+		int score = 1;
+		
+		for(int i=0; i<n; i++) {
+			
+			array[i] = br.readLine();
+			String[] temp = array[i].split(""); // 이럴 필요 없이, String값 그대로 chatAt() 써도 됨 
+			
+			for(int j=0; j<temp.length; j++) {
+				
+				if(temp[j].equals("O")) {
+					sum += score;
+					score++;
+				} else {
+					score = 1; // X를 만나면 score를 1로 초기화
+				}
+				
+			}
+			
+			sb.append(sum+"\n");
+			sum = 0;
+			score = 1;
+			
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
+		
+	}
+}
