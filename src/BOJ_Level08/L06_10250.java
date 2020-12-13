4fd17af1bb39a6c8e53892bf8889fc41217a3f83
proudofch <proudofch@gmail.com>
2020-12-13 15:50:55 +0900

    백준 - 10250 보완

--- src/BOJ_Level08/L06_10250.java
+++ src/BOJ_Level08/L06_10250.java
@@ -9,26 +9,33 @@ public class L06_10250 {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
 		int n = Integer.parseInt(br.readLine());
 		
 		for(int i=0; i<n; i++) {
 			
 			st = new StringTokenizer(br.readLine());
 			
-			int H = Integer.parseInt(st.nextToken());
-			int W = Integer.parseInt(st.nextToken());
-			int N = Integer.parseInt(st.nextToken());
+			int H = Integer.parseInt(st.nextToken()); // 높이
+			int W = Integer.parseInt(st.nextToken()); // 너비
+			int N = Integer.parseInt(st.nextToken()); // N번째 손님
 			
-			int floor = 0;
+			int floor = 1;
 			int unit = 1;
 			
-			if(N <= H) {
+			/* 처음 조건문을 N%H==0으로 걸어도 된다 */
+			if(N <= H) { // 이때는 다 1호이다.
 				floor = N;
-			} else {
-				floor += N/W-1;
-				unit += N%H;
+			} else { 
+				if(N % H == 0) {
+					floor = H;
+					unit = N/H;
+				} else {
+					floor = N%H;
+					unit += N/H;
+				}
 			}
 			
 			String unitStr = "";
@@ -45,6 +52,7 @@ public class L06_10250 {
 		}
 		
 		System.out.print(sb.toString());
+		
 		br.close();
 		
 	}
