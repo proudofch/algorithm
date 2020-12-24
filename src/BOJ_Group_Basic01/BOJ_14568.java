958ed28335c074ee83769551ff7332c6cb87cf8a
proudofch <proudofch@gmail.com>
2020-12-24 14:40:06 +0900

    백준 - 14568 보충

--- src/BOJ_Group_Basic01/BOJ_14568.java
+++ src/BOJ_Group_Basic01/BOJ_14568.java
@@ -3,52 +3,33 @@ package BOJ_Group_Basic01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class BOJ_14568 {
 	public static void main(String[] args) throws IOException {
 		
-		// 3명이 N개를 나누는 경우의 수 세기
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int candy = Integer.parseInt(br.readLine());
-		// 사탕은 1~100개
-		
-		List<Integer> list;
-		Set<List> set = new HashSet<List>();
-		
-		int a,b,c = 0;
-		
-		for(int i=2; i<=candy-3; i*=2) {
-			list = new ArrayList<Integer>();
-			a = i;
-			b = candy-i;
-			while(c < b+2) {
-				c++;
-				b -= 1;
-			}
-			if(a+b+c == candy) {
-				list.add(a);
-				list.add(b);
-				list.add(c);
-			}
-			set.add(list);
-		}
-		
 		int count = 0;
-		for(List l : set) {
-			if(!l.isEmpty()) {
-				count++;
+		
+		// 택희 영훈 남규
+		//  A  B  C
+ 		
+		for(int i=2; i<=candy; i+=2) { // 택희: 짝수 개의 사탕 (0개를 가지는 사람은 없단 조건이 있으므로 2부터 시작)
+			int temp = candy-i-2; // 남규는 영훈보다 2개 이상 많이 가져야하므로, 일단 2를 빼둔다.
+			for(int j=1; j<=temp; j++) { // 영훈
+				for(int k=temp; k>=1; k--) { // 남규
+					if(j+k == temp && j<=k) { // 영훈(j)의 사탕 개수는 남규(k)보다 적거나 같아야 한다(같으면 2개 차이)
+						System.out.println(i+"/"+j+"/"+k);
+						count++;
+					}
+				}
 			}
-			System.out.println(l);
 		}
+		
 		System.out.println(count);
 		
 		br.close();
-		
 		
 	}
 }
